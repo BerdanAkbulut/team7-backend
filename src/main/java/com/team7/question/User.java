9db97407a8148e6f0ae8c87717ca16b186a2947a
BerdanAkbulut <berdanakbulutt@hotmail.com>
2022-06-25 03:06:42 +0300

    fixing user model

--- src/main/java/com/team7/question/User.java
+++ src/main/java/com/team7/question/User.java
@@ -1,12 +1,20 @@
 package com.team7.question;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue
     private int id;
 
 
